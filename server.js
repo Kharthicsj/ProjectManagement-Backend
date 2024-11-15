dc0f458e732b1d20008d98bb5ab94471930f74cd
Kharthicsj <kharthicsj@gmail.com>
2024-11-15 11:31:49 +0530

    Ready for Launch

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ async function connectDB() {
 connectDB();
 
 // Middlewares
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000','https://project-management-frontend-magf.onrender.com'], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
